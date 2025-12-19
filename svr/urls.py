dd76a93cbad8a7e37ae4e5d84ca65b9488c6eb42
koich <koich@likids.info>
2025-12-19 14:07:06 +0900

    Add minimal placeholder top page using TemplateView

--- svr/urls.py
+++ svr/urls.py
@@ -16,8 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import TemplateView
 
 urlpatterns = [
+    path(
+        '',
+        TemplateView.as_view(template_name='dictionary/home.html'),
+        name='home'
+    ),
     path('admin/', admin.site.urls),
     path('replace/', include('dictionary.urls')),
     path('', include('api.urls')),
